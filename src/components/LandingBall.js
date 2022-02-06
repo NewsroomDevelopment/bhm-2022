a32748238537bd62027d0b106605f0106e500e6c
vg2425 <vg2425@columbia.edu>
2022-02-06 15:48:22 -0500

    landingball

--- src/components/LandingBall.js
+++ src/components/LandingBall.js
@@ -1,11 +1,29 @@
 import React from "react";
 import styled from "styled-components";
 
-const LandingBall = ({}) => {
+
+const Ball = styled.div`
+  height: 4vw;
+  width: 4vw;
+  padding: 2vw;
+  background-color: #bbb;
+  border-radius: 50%;
+  display: inline-block;
+    }
+`;
+
+const ImageWrap = styled.div`
+  width: 1.5vw;
+  height: 1.5vw;
+  transition: opacity 1.5s;
+  opacity: ${({ props }) => (props.trigger ? '1' : '0')};
+`;
+
+const LandingBall = ({ article, trigger }) => {
   return (
-    <>
-    
-    </>
+    <ImageWrap trigger={trigger}>
+      <Ball src={article.image_url} />
+    </ImageWrap>
   );
 };
 

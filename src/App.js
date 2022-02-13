e07f55aaa82dc365a6ee7ea7d2c1ad04f690f04b
vg2425 <vg2425@columbia.edu>
2022-02-13 15:05:16 -0500

    letter to editr

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import { GlobalStyles, Theme } from "./utils/Styles";
 import Landing from "./containers/Landing";
 import ArticleSection from "./containers/ArticleSection";
 import TimelineContainer from "./containers/TImelineContainer";
+import LetterSec from './containers/LetterEditor';
+
 function App() {
   return (
     <ThemeProvider theme={Theme}>
       <GlobalStyles />
       <Landing />
+      <LetterSec />
       <ArticleSection />
       <TimelineContainer></TimelineContainer>
     </ThemeProvider>

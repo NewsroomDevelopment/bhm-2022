6faa15acf80eedb7fe8d704497a812c1effbb2db
el3031 <67562724+el3031@users.noreply.github.com>
2022-02-16 20:50:34 -0500

    more timeline heading stuff

--- src/components/TimelineHeading.js
+++ src/components/TimelineHeading.js
@@ -2,10 +2,11 @@ import react from "react";
 import styled from "styled-components";
 
 const TimelineHeadingStyle = styled.div`
-    margin-left: ${props => props.side === "left" ? "100px" : "-20px"};
-    margin-right: ${props => props.side === "left" ? "-20px" : "100px"};;
+    margin-left: ${props => props.side === "left" ? "200px" : "-20px"};
+    margin-right: ${props => props.side === "left" ? "-20px" : "200px"};;
     border-bottom: 5px solid orange;
     padding: 20px;
+    text-align: left;
 `
 
 const TimelineHeading = ({section, side}) => {
